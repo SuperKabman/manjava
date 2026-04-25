43c736df48463da5a2db70b39723e163b8568692
Kabir <109855930+SuperKabman@users.noreply.github.com>
2026-04-25 13:48:26 -0700

    added Pit.java, Mancala.java

---
+++ Pit.java
@@ -0,0 +1,46 @@
+
+public class Pit {
+    private int stones;
+
+    public Pit() {
+        this.stones = 0;
+    }
+
+    public Pit(int initialStones) {
+        if (initialStones < 0) throw new IllegalArgumentException("stone count can't be negative.");
+        this.stones = initialStones;
+    }
+
+    public int getStones() {
+        return stones;
+    }
+
+
+    public int addStones(int stones) {
+        if (stones < 0) throw new IllegalArgumentException("can't add negative stones.");
+        this.stones += stones;
+        return this.stones;
+    }
+
+
+    public int removeAllStones() {
+        int removed = this.stones;
+        this.stones = 0;
+        return removed;
+    }
+
+    public boolean isEmpty() {
+        return stones == 0;
+    }
+
+    public int setStones(int stones) {
+        if (stones < 0) throw new IllegalArgumentException("stone count can't be negative.");
+        this.stones = stones;
+        return this.stones;
+    }
+
+    @Override
+    public String toString() {
+        return "Pit[" + stones + "]";
+    }
+}
